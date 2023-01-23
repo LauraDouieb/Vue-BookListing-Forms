a107c7776645b749465431193ed9168b04270240
Laura <laura.stievenard@capgemini.com>
2023-01-23 14:31:11 +0100

    filter

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,14 +2,25 @@
   <div>
     <h1>{{title}}</h1>
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+        <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
+    <hr>
+    <h2>
+      Filtered Books By Ownership
+    </h2>
+    <select v-model="holding">
+      <ul>
+        <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+      </ul>
+      <option v-for='filter in filters'>{{ filter }}</option>
+    </select>
   </div>
 </template>
 
 <script>
+  import _ from "lodash";
   import BookItem from "./BookItem";
   import BookForm from "./BookForm";
 
@@ -20,11 +31,30 @@
         title: "All Books",
         states: ["Want to Read", "Read", "Reading"],
         books: [
-          { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true},
-          { title: "American Gods", author: "Neil Gaiman", finishedReading: false},
-          { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true}
+          { 
+            title: "Self-Reliance", 
+            author: "Ralph Waldo Emerson", 
+            finishedReading: true,
+            ownership: "borrowed"
+          },
+          { 
+            title: "American Gods", 
+            author: "Neil Gaiman", 
+            finishedReading: false,
+            ownership: "bought"
+          },          { 
+            title: "Amusing Ourselves to Death", 
+            author: "Neil Postman", 
+            finishedReading: true,
+            ownership: "borrowed"
+          }
         ]
       };
+    },
+    computed: {
+      filteredBooks() {
+        return _.filter(this.books, ["ownership", this.holding]);
+      }
     },
     components: {
       BookItem,
@@ -35,23 +65,24 @@
         this.books.push({
           title: bookData.bookTitle,
           author: bookData.bookAuthor,
-          finishedReading: bookData.finishedReading
+          finishedReading: bookData.finishedReading,
+          ownership: bookData.ownership
         });
       }
-    }
+    },
+    filters: ["bought", "borrowed"],
+    holding: "bought"
   };
 </script>
 
 <style>
-h1,
-h2 {
+h1, h2 {
   font-weight: normal;
 }
 ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: block;
   margin: 0 10px;
